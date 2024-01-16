d5e22bc1c6c3dd30e7aae3be48e864f14355406d
Berk Şahin <86800475+eberksahin@users.noreply.github.com>
2024-01-16 16:42:19 +0300

    Food In Basket
    - Assets added.
    - Scenes added. (Menu, Main)
    - UI Menu.cs script was created.

---
+++ Food In Basket/Assets/Scripts/UIMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenu : MonoBehaviour
+{
+    private TextMeshProUGUI startButton;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
